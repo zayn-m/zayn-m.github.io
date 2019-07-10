b2d00df671db9070c4a0b10ab0bc4d01b1be760a
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-07-10 22:19:26 +0500

    initial commit

---
+++ assets/js/theme.js
@@ -0,0 +1,45 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+    
+    
+$(function() {
+    $('a.js-scroll-trigger').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 56
+  });
+
+  // Collapse Navbar
+  var navbarCollapse = function() {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-light");
+    } else {
+      $("#mainNav").removeClass("navbar-light");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+ 
+
+})(jQuery); // End of use strict
